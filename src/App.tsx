2c7c15d475bfb7a33ed466a7efe0ccbd19c65255
dalmia01 <dalmia01@gmail.com>
2021-10-24 10:29:53 +0530

    display - player values - game state

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Button } from './Components/Button';
-import { Input } from './Components/Input';
-import { Title } from './Components/Title';
+import React, { useState } from "react";
+import "./App.css";
+import { Button } from "./Components/Button";
+import { Input } from "./Components/Input";
+import { Title } from "./Components/Title";
 
 function App() {
+    const [boardNumber, setBoardNumber] = useState(3);
+    const [finalBoard, setFinalBoard] = useState(3);
+    const [playerValues, setPlayerValues] = useState({
+        player1: 0,
+        player2: 0,
+    });
+    const [winner, setWinner] = useState(null);
 
-  const [boardNumber,setBoardNumber] = useState(3);
-  const [finalBoard,setFinalBoard] = useState(3);
+    const changeBoardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (isNaN(Number(e.target.value))) return;
+        setBoardNumber(Number(e.target.value));
+    };
 
-  const changeBoardNumber = (e:React.ChangeEvent<HTMLInputElement>) => {
-    if(isNaN(Number(e.target.value))) return;
-    setBoardNumber(Number(e.target.value));
-  }
+    const changeBoardHandler = () => {
+        if (boardNumber < 2 || boardNumber > 8) return;
+        setFinalBoard(boardNumber);
+    };
 
-  const changeBoardHandler = () => {
-    if(boardNumber < 2 || boardNumber > 8) return ;
-    setFinalBoard(boardNumber);
-  }
+    const resetHandler = () => {
+        setBoardNumber(3);
+        setFinalBoard(3);
+        setPlayerValues({ player1: 0, player2: 0 });
+        setWinner(null);
+    };
 
-  const resetHandler = () => {
-    setBoardNumber(3);
-    setFinalBoard(3);
-}
-
-  return (
-    <main className="App">
-      <Title title={'Encora Board Game'} />
-      <Input value={boardNumber} changeValue={changeBoardNumber} placeholder={'Select Board Type - (2- 8)'}  />
-      <Button text={'Set Board'} clickHandler={changeBoardHandler} disabled={boardNumber < 2 || boardNumber > 8} />
-      <div><Button text={'Reset'} clickHandler={resetHandler} /></div> 
-    </main>
-  );
+    return (
+        <main className="App">
+            <Title title={"Encora Board Game"} />
+            <Input
+                value={boardNumber}
+                changeValue={changeBoardNumber}
+                placeholder={"Select Board Type - (2- 8)"}
+            />
+            <Button
+                text={"Set Board"}
+                clickHandler={changeBoardHandler}
+                disabled={boardNumber < 2 || boardNumber > 8}
+            />
+            <div className="player-values">
+                <div>Player 1 : {playerValues.player1} </div>
+                <div>Player 2 : {playerValues.player2} </div>
+                {winner && <div>winner : {winner}</div>}
+            </div>
+            <div>
+                <Button text={"Reset"} clickHandler={resetHandler} />
+            </div>
+        </main>
+    );
 }
 
 export default App;
